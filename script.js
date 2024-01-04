9b75090d0ceaecc8ba1575d9edf2e1324f5505a9
DavidRodriguez119 <santiago1.dsrr@gmail.com>
2024-01-04 03:28:02 -0400

    get and display 5-day forecast

--- script.js
+++ script.js
@@ -4,14 +4,19 @@ const APIKey = `5b4acd3e0ae2a7e29332200294faf228`;
 // Global Variables
 let city;
 let cityData;
+const forecast = [];
 const inputField = document.getElementById(`input-field`);
 const cityName = document.getElementById(`city-name`);
 const currentTemp = document.getElementById(`current-temp`);
 const currentWind = document.getElementById(`current-wind`);
 const currentHumidity = document.getElementById(`current-humidity`);
+const forecastContainer = document.getElementById(`forecast-container`);
 
 // when the search button is clicked do the following:
 document.getElementById(`search-button`).addEventListener(`click`, function () {
+    // reset page
+    forecast.length = 0;
+    forecastContainer.innerHTML = ``
     // get the input value
     city = inputField.value
     // Fetch Request
@@ -23,18 +28,82 @@ document.getElementById(`search-button`).addEventListener(`click`, function () {
         cityData = data;
         console.log(cityData);
         setCurrentValues();
+        getForecastData();
         });        
 })
 
 // Set the values of the current day into the page
 const setCurrentValues = () => {
     const day = dayjs().format('DD/MM/YYYY');
-    cityName.innerHTML = `${cityData.name} (${day})`;
+    let emojiHeader;
+    if(cityData.weather[0].main === `Clear`){
+        emojiHeader = `☀️`;
+    } else if (cityData.weather[0].main === `Clouds`){
+        emojiHeader = `☁️`;
+    } else {
+        emojiHeader = `☔`;
+    }
+    cityName.innerHTML = `${cityData.name} (${day}) ${emojiHeader}`;
     tempCelcius = cityData.main.temp-273.15;
     currentTemp.innerHTML = `Temp: ${tempCelcius.toFixed(2)}°C`;
     currentWind.innerHTML = `Wind: ${cityData.wind.speed} MPH`;
     currentHumidity.innerHTML = `Humidity: ${cityData.main.humidity}%`
-    
+};
+
+// get data of 5 day forecast
+const getForecastData = () => {
+    const requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${cityData.coord.lat}&lon=${cityData.coord.lon}&appid=${APIKey}&cnt=40`
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            forecast.push(data.list[0]);
+            forecast.push(data.list[8]);
+            forecast.push(data.list[16]);
+            forecast.push(data.list[24]);
+            forecast.push(data.list[32]);
+            console.log(forecast);
+            setForecastValues();
+        });
+};
+
+const setForecastValues = () => {
+    for(let i = 0; i <= 4; i++){        
+        const forecastDiv = document.createElement(`div`);
+        forecastDiv.classList.add(`forecast-div`);
+        // create html elements
+        const subTitle = document.createElement(`h3`);
+        const emoji = document.createElement(`p`);
+        const tempParagraph = document.createElement(`p`);
+        const windParagraph = document.createElement(`p`);
+        const humidityParagraph = document.createElement(`p`);
+
+        // add content
+        subTitle.textContent = dayjs().add(i+1, `day`).format('DD/MM/YYYY');
+        if(forecast[i].weather[0].main === `Clear`){
+            emoji.textContent = `☀️`;
+        } else if (forecast[i].weather[0].main === `Clouds`){
+            emoji.textContent = `☁️`;
+        } else {
+            emoji.textContent = `☔`;
+        }
+        tempCelcius = forecast[i].main.temp-273.15;
+        tempParagraph.textContent = `Temp: ${tempCelcius.toFixed(2)}°C`;
+        windParagraph.textContent = `Wind: ${forecast[i].wind.speed}MPH`;
+        humidityParagraph.textContent = `Humidity: ${forecast[i].main.humidity}%`;
+
+        // append to the page
+        forecastContainer.appendChild(forecastDiv);
+        forecastDiv.appendChild(subTitle);
+        forecastDiv.appendChild(emoji);
+        forecastDiv.appendChild(tempParagraph);
+        forecastDiv.appendChild(windParagraph);
+        forecastDiv.appendChild(humidityParagraph);
+
+     }
 }
+
+
 
 
